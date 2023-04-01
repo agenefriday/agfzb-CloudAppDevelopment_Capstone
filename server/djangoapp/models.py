c0a14f58250502650040792377fda295dcfedc01
agenefriday <122647054+agenefriday@users.noreply.github.com>
2023-04-01 11:22:25 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,13 +10,11 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 class CarMake(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
-    desc = models.CharField(max_length=200)
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(max_length=280)
 
     def __str__(self):
         return self.name
-
-
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -26,21 +24,28 @@ class CarMake(models.Model):
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
 class CarModel(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
-    id = models.CharField(max_length=2)
-    type = models.CharField(max_length=10)
-    year = models.DateField(null=True)
-    carmakes = models.ManyToManyField(CarMake)
+    #constant for c_type choices
+    TYPES = (
+            ("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon"), ("LIMOUSINE", "Limousine"), ("BATMOBILE", "Batmobile")
+        )
+
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    c_type = models.CharField(max_length=30, choices=TYPES)
+    dealer_id = models.IntegerField()
+    year = models.DateField()
 
     def __str__(self):
-        return self.name
-
+        return "Name: " + self.name + \
+                " Make Name: "+ self.make.name + \
+                " Type: " + self.c_type + \
+                " Dealer ID: " + str(self.dealer_id)+ \
+                " Year: " + str(self.year)
+                
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
 class CarDealer:
 
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
@@ -66,33 +71,20 @@ class CarDealer:
     def __str__(self):
         return "Dealer name: " + self.full_name
 
-
-
 # <HINT> Create a plain Python class `DealerReview` to hold review data
-
 class DealerReview:
-
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
-        # Dealership
-        self.dealership = dealership
-        # Dealer name
-        self.name = name
-        # Dealer purchase
-        self.purchase = purchase
-        # Dealer review
-        self.review = review
-        # purchase_date
-        self.purchase_date = purchase_date
-        # car_make
-        self.car_make = car_make
-        # car_model
-        self.car_model = car_model
-        # car_year
-        self.car_year = car_year
-        # sentiment
-        self.sentiment = sentiment
-        # id
-        self.id = id
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
+        self.dealership=dealership
+        self.name=name
+        self.purchase=purchase
+        self.review=review
+        self.purchase_date=purchase_date
+        self.car_make=car_make
+        self.car_model=car_model
+        self.car_year=car_year
+        self.sentiment=sentiment #Watson NLU service
+        self.id=id
 
     def __str__(self):
-        return "Dealer name: " + self.full_name
+        return "Review: " + self.review +\
+                " Sentiment: " + self.sentiment
